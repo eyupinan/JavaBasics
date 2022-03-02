d774b7fd137b523be13bbceabb47469dc79bc4db
İbrahim Eyyüp İNAN <eyyupinan0@gmail.com>
2022-03-02 13:01:50 +0300

    vücut kitle indexi

---
+++ src/main/java/kitleIndex/main.java
@@ -0,0 +1,16 @@
+package kitleIndex;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        double kilo,boy,index ;
+        System.out.print("Kilonuzu giriniz: ");
+        kilo = input.nextDouble();
+        System.out.print("boyunuzu meetre cinsinden giriniz: ");
+        boy = input.nextDouble();
+        index = kilo / Math.pow(boy,2);
+        System.out.println("Vücut kitle Ýndeksiniz : " + index);
+    }
+}
