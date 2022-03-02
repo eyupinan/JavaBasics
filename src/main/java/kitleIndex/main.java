4ebe0da3a7ff279fd054a4d829186bd28b7a5b17
İbrahim Eyyüp İNAN <eyyupinan0@gmail.com>
2022-03-02 13:11:27 +0300

    manav

--- src/main/java/kitleIndex/main.java
+++ src/main/java/kitleIndex/main.java
@@ -3,14 +3,38 @@ package kitleIndex;
 import java.util.Scanner;
 
 public class main {
+    public enum prices{
+        ARMUT(2.14),
+        ELMA(3.67),
+        DOMATES(1.11),
+        MUZ(0.95),
+        PATLICAN(5);
+        private final double price;
+
+        private prices(double price) {
+            this.price = price;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        double kilo,boy,index ;
-        System.out.print("Kilonuzu giriniz: ");
-        kilo = input.nextDouble();
-        System.out.print("boyunuzu meetre cinsinden giriniz: ");
-        boy = input.nextDouble();
-        index = kilo / Math.pow(boy,2);
-        System.out.println("Vücut kitle Ýndeksiniz : " + index);
+        double armut,elma,domates,muz,patlıcan , tutar ;
+        System.out.print("Armut kaç kilo? : ");
+        armut = input.nextDouble();
+        System.out.print("Elma kaç kilo? : ");
+        elma = input.nextDouble();
+        System.out.print("Domates kaç kilo? : ");
+        domates = input.nextDouble();
+        System.out.print("Muz kaç kilo? : ");
+        muz = input.nextDouble();
+        System.out.print("Patlıcan kaç kilo? : ");
+        patlıcan = input.nextDouble();
+        tutar = armut * prices.ARMUT.getPrice()+elma*prices.ELMA.getPrice()+domates*prices.DOMATES.getPrice()
+                + muz*prices.MUZ.getPrice()+patlıcan*prices.PATLICAN.getPrice();
+        System.out.println("Tutar:"+tutar);
+
     }
 }
