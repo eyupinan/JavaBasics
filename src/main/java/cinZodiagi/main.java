bb816ab3f76af227f12ee8d2ade6cf7654660966
İbrahim Eyyüp İNAN <eyyupinan0@gmail.com>
2022-03-02 21:43:51 +0300

    çin zodyağı

---
+++ src/main/java/cinZodiagi/main.java
@@ -0,0 +1,50 @@
+package cinZodiagi;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Doğum yılınızı giirn :");
+        int dogum = input .nextInt();
+        switch (dogum%12){
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+
+        }
+    }
+}
